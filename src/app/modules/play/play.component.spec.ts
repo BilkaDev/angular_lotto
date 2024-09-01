02926bc802260044ef9c96f4fe3b30616e55c021
Marcin Bilka <bilkadev@gmail.com>
2024-09-01 13:35:51 +0200

    feat: added content to play component and style

--- src/app/modules/play/play.component.spec.ts
+++ src/app/modules/play/play.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { PlayComponent } from './play.component';
+import { PlayComponent } from "./play.component";
 
-describe('PlayComponent', () => {
+describe("PlayComponent", () => {
   let component: PlayComponent;
   let fixture: ComponentFixture<PlayComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PlayComponent]
-    })
-    .compileComponents();
+      imports: [PlayComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(PlayComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
--- src/app/modules/play/play.component.ts
+++ src/app/modules/play/play.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 
 @Component({
-  selector: 'app-play',
+  selector: "app-play",
   standalone: true,
-  imports: [],
-  templateUrl: './play.component.html',
-  styleUrl: './play.component.scss'
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  templateUrl: "./play.component.html",
+  styleUrl: "./play.component.scss",
 })
-export class PlayComponent {
-
-}
+export class PlayComponent {}
