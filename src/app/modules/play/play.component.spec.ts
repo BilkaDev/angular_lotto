4eb303af4884a8cf96b14fd74173889e763ccc70
Marcin Bilka <bilkadev@gmail.com>
2024-09-01 16:17:33 +0200

    feat: added random numbers, validate numbers, snackbar and ngmodel

--- src/app/modules/play/play.component.spec.ts
+++ src/app/modules/play/play.component.spec.ts
@@ -1,14 +1,22 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { PlayComponent } from "./play.component";
+import { TranslateTestingModule } from "ngx-translate-testing";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
+import enJson from "../../../../public/i8n/en.json";
+
+const translateTestingModule = () =>
+  TranslateTestingModule.withTranslations({
+    enJson,
+  });
 describe("PlayComponent", () => {
   let component: PlayComponent;
   let fixture: ComponentFixture<PlayComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [PlayComponent],
+      imports: [PlayComponent, translateTestingModule(), BrowserAnimationsModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(PlayComponent);
--- src/app/modules/play/play.component.ts
+++ src/app/modules/play/play.component.ts
@@ -3,12 +3,50 @@ import { MatCardModule } from "@angular/material/card";
 import { MatButtonModule } from "@angular/material/button";
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
+import { FormsModule } from "@angular/forms";
+import { NgForOf } from "@angular/common";
+import { TranslateService } from "@ngx-translate/core";
+
+import { SnackbarService } from "../shared/ui/snackbar/snackbar.service";
+import { TicketDate } from "../core/models/ticket.model";
+
+import { ValidateService } from "./validate.service";
+import { RandomNumbersService } from "./random-numbers.service";
 
 @Component({
   selector: "app-play",
   standalone: true,
-  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule],
+  imports: [MatCardModule, MatButtonModule, MatFormFieldModule, MatInputModule, FormsModule, NgForOf],
   templateUrl: "./play.component.html",
   styleUrl: "./play.component.scss",
 })
-export class PlayComponent {}
+export class PlayComponent {
+  ticketData: TicketDate = { numbers: [] };
+
+  constructor(
+    private validateService: ValidateService,
+    private snackbar: SnackbarService,
+    private translate: TranslateService,
+    private randomNumberService: RandomNumbersService
+  ) {}
+
+  updateNumber(index: number, value: string) {
+    const num = Number(value);
+    if (this.validateService.validateNumber(num)) {
+      this.ticketData.numbers[index] = num;
+    }
+  }
+
+  onPlay() {
+    const errorMessage = this.validateService.validateNumbers(this.ticketData.numbers);
+    if (errorMessage) {
+      const translatedErrorMessage = this.translate.instant(errorMessage);
+      this.snackbar.openSnackBar(translatedErrorMessage, true);
+      return;
+    }
+  }
+  setRandomNumbers() {
+    const numbers = this.randomNumberService.generateRandomNumbers(6, 1, 99);
+    this.ticketData = { numbers };
+  }
+}
