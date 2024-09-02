70113a0a0eb9f93575baf74225f51a17cc9c7fac
Marcin Bilka <bilkadev@gmail.com>
2024-09-02 19:24:14 +0200

    feat: added ticket services

---
+++ src/app/modules/play/ticket.service.spec.ts
@@ -0,0 +1,25 @@
+import { TestBed } from "@angular/core/testing";
+
+import { TicketService } from "./ticket.service";
+import { HttpClientModule } from "@angular/common/http";
+import { TranslateTestingModule } from "ngx-translate-testing";
+import enJson from "../../../../public/i8n/en.json";
+
+const translateTestingModule = () =>
+  TranslateTestingModule.withTranslations({
+    enJson,
+  });
+describe("TicketService", () => {
+  let service: TicketService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, translateTestingModule()],
+    });
+    service = TestBed.inject(TicketService);
+  });
+
+  it("should be created", () => {
+    expect(service).toBeTruthy();
+  });
+});
