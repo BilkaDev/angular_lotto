77a307425345941920d999e0a116f13d4513725b
Marcin Bilka <bilkadev@gmail.com>
2024-09-05 19:55:16 +0200

    feat: added displey hit and won numbers condition

--- src/app/modules/results/components/result-message/result-message.component.ts
+++ src/app/modules/results/components/result-message/result-message.component.ts
@@ -1,10 +1,10 @@
 import { Component, Input } from "@angular/core";
 import { MatIcon } from "@angular/material/icon";
 import { TranslateModule } from "@ngx-translate/core";
+import { NgIf } from "@angular/common";
 
 import { Result } from "../../../core/models/result.model";
 import { DatePipe } from "../../../core/pipes/date.pipe";
-import { NgIf } from "@angular/common";
 
 @Component({
   selector: "app-result-message",
