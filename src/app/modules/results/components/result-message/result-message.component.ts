e8aeba97638cc7aac78ff131846c0fc1053dbce7
Marcin Bilka <bilkadev@gmail.com>
2024-09-05 16:25:35 +0200

    feat: added result message comp

---
+++ src/app/modules/results/components/result-message/result-message.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from "@angular/core";
+import { MatIcon } from "@angular/material/icon";
+import { TranslateModule } from "@ngx-translate/core";
+
+import { Result } from "../../../core/models/result.model";
+import { DatePipe } from "../../../core/pipes/date.pipe";
+
+@Component({
+  selector: "app-result-message",
+  standalone: true,
+  imports: [TranslateModule, DatePipe, MatIcon],
+  templateUrl: "./result-message.component.html",
+  styleUrl: "./result-message.component.scss",
+})
+export class ResultMessageComponent {
+  @Input() result!: Result;
+}
