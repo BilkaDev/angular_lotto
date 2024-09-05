c2af36c2e8603849b2bf18b60d7579c6f5ba40b8
Marcin Bilka <bilkadev@gmail.com>
2024-09-05 16:04:33 +0200

    fix: fixed request termination reading.

--- src/app/modules/core/interceptors/spinner.interceptor.ts
+++ src/app/modules/core/interceptors/spinner.interceptor.ts
@@ -1,17 +1,11 @@
-import { HttpEventType, HttpInterceptorFn } from "@angular/common/http";
+import { HttpInterceptorFn } from "@angular/common/http";
 import { inject } from "@angular/core";
-import { tap } from "rxjs";
+import { finalize } from "rxjs";
 
 import { SpinnerService } from "../services/spinner.service";
 
 export const spinnerInterceptor: HttpInterceptorFn = (req, next) => {
   const spinnerService = inject(SpinnerService);
   spinnerService.show();
-  return next(req).pipe(
-    tap((event) => {
-      if (event.type === HttpEventType.Response) {
-        spinnerService.hide();
-      }
-    })
-  );
+  return next(req).pipe(finalize(() => spinnerService.hide()));
 };
